8ef7be9ff75cede18229ff937b90b157fbd9add0
bobozhangshao <1310255342@qq.com>
2016-12-02 21:20:32 +0800

    measure

--- actions/measure.php
+++ actions/measure.php
@@ -5,8 +5,25 @@
  * @Time: 2016/11/21 10:01
  * @Desc: store the measure data into a text file
  */
-function writeDataTxt(){
 
+function writeDataTxt(){
+    date_default_timezone_set('Asia/Shanghai');
+    $dataPiece['username']    = $_POST['username'];
+    $dataPiece['data_type']   = $_POST['type'];
+    $dataPiece['device_type'] = $_POST['device'];
+    $dataPiece['data_piece']  = $_POST['data'];
+
+    $minute = ((int)(date("i",'1480682827')/6))*6;
+    $dataPiece['time_pre']  = date("Y_m_d_H_",$_POST['time']);
+    $dataPiece['time_tail'] = date("_s",$_POST['time']);
+
+    $dataPiece['time'] = $dataPiece['time_pre'].$minute.$dataPiece['time_tail'];
+
+    $filename = $dataPiece['username'].'_'.$dataPiece['device_type'].'_'.$dataPiece['data_type'].'_'.$dataPiece['time'];
+    $file     = substr(__DIR__,0,-7)."data/".$_GET['username']."/".$filename;
+
+    $res = file_put_contents($file,$dataPiece['data_piece'],FILE_APPEND);
+    return $res;
 }
 
 echo writeDataTxt();
