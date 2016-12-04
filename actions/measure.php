b0fb788e72172a473786419e4116ac794ea34978
bobozhangshao <1310255342@qq.com>
2016-12-04 19:01:09 +0800

    优化文件时间戳

--- actions/measure.php
+++ actions/measure.php
@@ -13,17 +13,39 @@ function writeDataTxt(){
     $dataPiece['device_type'] = $_POST['device'];
     $dataPiece['data_piece']  = implode("\n",json_decode($_POST['data'],true))."\n";
 
-	
-    $minute = ((int)(date("i",$_POST['time'])/6))*6;
-    $dataPiece['time_pre']  = date("Y_m_d_H_",$_POST['time']);
+    $timeStamp = $_POST['time'];
+    $dataPiece['time'] = timeInDocument($timeStamp,$dataPiece['username']);
 
-    $dataPiece['time'] = $dataPiece['time_pre'].$minute.'_00';
-
-    $filename = $dataPiece['username'].'_'.$dataPiece['device_type'].'_'.$dataPiece['data_type'].'_'.$dataPiece['time'];
-	$file     = substr(__DIR__,0,-7)."data\/".$_POST['username']."\/".$filename;  
+    $filename = $dataPiece['username'].'_'.$dataPiece['device_type'].'_'.$dataPiece['data_type'].'_'.$dataPiece['time'].'.txt';
+	$file     = substr(__DIR__,0,-7).'data\/'.$_POST['username'].'\/'.$filename;
 
     $res = file_put_contents($file,$dataPiece['data_piece'],FILE_APPEND);
     return $res;
+}
+
+function timeInDocument($timeStamp,$username){
+    $dir = substr(__DIR__,0,-7).'data\/'.$username;
+    $files = array_diff(scandir($dir),array('.','..','index.html'));
+
+    $result = array();
+    if ($files){
+        $count = 0;
+        foreach ($files as $item){
+            $arr = explode('_',substr($item,0,strpos($item,'.')));
+            $time = $arr[3].'.'.$arr[4].'.'.$arr[5].' '.$arr[6].':'.$arr[7].':'.$arr[8];
+            $result[$count] = strtotime($time);
+        }
+    }
+
+    $minute = ((int)(date("i",$timeStamp)/6))*6;
+    $timePre  = date("Y_m_d_H_",$timeStamp);
+    $time =  $timePre.$minute.'_00';
+
+    if (key_exists($time,$result)){
+        return $time;
+    }
+
+    return $timeStamp;
 }
 
 echo writeDataTxt();
