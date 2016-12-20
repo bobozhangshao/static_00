9bd4e08ebd546b033c3ae4fcc04d08db05159ce2
bobozhangshao <1310255342@qq.com>
2016-12-20 10:57:40 +0800

    clear

---
+++ actions/waveshow.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Created by PhpStorm.
+ * @User: zhangshaobo | bobozhangshao@gmail.com
+ * @Time: 2016/12/19 14:01
+ * @Desc: 提供波形数据
+ */
+//组合数据
+//function ($file, $type){
+//
+//}
+date_default_timezone_set('Asia/Shanghai');
